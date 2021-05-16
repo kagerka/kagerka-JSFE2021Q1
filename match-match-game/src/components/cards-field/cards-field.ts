a151748b74fcf1592d0372ed8588223bd23da648
kagerka <kagerka@gmail.com>
2021-05-16 12:52:33 +0300

    init: add cards-field files

---
+++ match-match-game/src/components/cards-field/cards-field.ts
@@ -0,0 +1,26 @@
+import './cards-field.scss';
+import { BaseComponent } from '../base-components';
+import { Card } from '../card/card';
+
+const SHOW_TIME = 1;
+
+export class CardsField extends BaseComponent {
+  private cards: Card[] = [];
+
+  constructor() {
+    super('div', ['cards-field']);
+  }
+
+  clear(): void {
+    this.cards = [];
+    this.element.innerHTML = '';
+  }
+
+  addCards(cards: Card[]): void {
+    this.cards = cards;
+    this.cards.forEach((card) => this.element.appendChild(card.element));
+    setTimeout(() => {
+      this.cards.forEach((card) => card.flipToBack());
+    }, SHOW_TIME * 1000);
+  }
+}
