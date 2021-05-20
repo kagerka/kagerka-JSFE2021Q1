5f541bac173ec39d6bf15f624079a3fcd22b12c7
kagerka <kagerka@gmail.com>
2021-05-20 15:46:07 +0300

    fix: fix game field component

---
+++ match-match-game/src/components/game-field.ts
@@ -0,0 +1,19 @@
+import { Game } from './game/game';
+import { ImageCategoryModel } from '../models/image-category-model';
+
+export class GameField {
+  private readonly game: Game;
+
+  constructor(private readonly rootElement: HTMLElement) {
+    this.game = new Game();
+    this.rootElement.appendChild(this.game.element);
+  }
+
+  async start(): Promise<void> {
+    const res = await fetch('./images.json');
+    const categories: ImageCategoryModel[] = await res.json();
+    const cat = categories[0];
+    const images = cat.images.map((name) => `${cat.category}/${name}`);
+    this.game.newGame(images);
+  }
+}
