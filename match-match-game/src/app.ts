5a2917b1f04f1375d68c35c1950107824ac99f54
kagerka <kagerka@gmail.com>
2021-05-20 15:46:40 +0300

    fix: fix app file

--- match-match-game/src/app.ts
+++ match-match-game/src/app.ts
@@ -1,19 +1,45 @@
-import { Game } from './components/game/game';
-import { ImageCategoryModel } from './models/image-category-model';
+// import { Game } from './components/game/game';
+// import { ImageCategoryModel } from './models/image-category-model';
+import { BaseComponent } from './components/base-components';
+import { AboutPage } from './pages/about';
+import { GameField } from './components/game-field';
 
-export class App {
-  private readonly game: Game;
+export class App extends BaseComponent {
+  private readonly main: HTMLElement;
 
   constructor(private readonly rootElement: HTMLElement) {
-    this.game = new Game();
-    this.rootElement.appendChild(this.game.element);
+    super('main', ['game-field']);
+    this.main = document.createElement('main');
   }
 
-  async start(): Promise<void> {
-    const res = await fetch('./images.json');
-    const categories: ImageCategoryModel[] = await res.json();
-    const cat = categories[0];
-    const images = cat.images.map((name) => `${cat.category}/${name}`);
-    this.game.newGame(images);
+  clear(): void {
+    this.element.innerHTML = '';
+  }
+
+  render(): HTMLElement {
+    this.rootElement.appendChild(this.element);
+    new AboutPage(this.element).render();
+
+    const registerButton = document.querySelector('.game__register_button');
+    const registerForm = document.querySelector('.overlay');
+
+    registerButton?.addEventListener('click', () => {
+        registerForm?.classList.remove('hidden');
+    });
+
+    registerForm?.addEventListener('click', (event) => {
+      if (event.target) {
+        if ((event.target as Element).classList.contains('overlay') || (event.target as Element).classList.contains('cancel')) {
+          registerForm.classList.add('hidden');
+        }
+        if ((event.target as Element).classList.contains('add-user')) {
+          registerForm.classList.add('hidden');
+          this.element.innerHTML = '';
+          new GameField(this.element).start();
+        }
+      }
+    });
+
+    return this.main;
   }
 }
