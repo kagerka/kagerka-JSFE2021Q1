80920d0975e5cab6189d6682cd2958a2bcb63dc1
kagerka <kagerka@gmail.com>
2021-05-20 15:45:36 +0300

    fix: fix header components

--- match-match-game/src/components/header/header.ts
+++ match-match-game/src/components/header/header.ts
@@ -1,32 +1,28 @@
 import './header.scss';
 import { BaseComponent } from '../base-components';
-import { HeaderMenu } from '../header_menu/menu';
-import { HeaderGame } from '../header_game/header_game';
+import { HeaderLogo } from './header_logo/logo';
+import { HeaderMenu } from './header_menu/menu';
+import { HeaderGame } from './header_game/header_game';
 
 export class Header extends BaseComponent {
   private readonly header: HTMLElement;
 
-  private headerMenu: HeaderMenu;
+  private headerLogo: HeaderLogo;
 
-  private headerGame: HeaderGame;
+  private headerMenu: HeaderMenu;
 
   constructor(private readonly rootElement: HTMLElement) {
     super('header', ['header']);
     this.header = document.createElement('header');
+    this.headerLogo = new HeaderLogo(this.element);
     this.headerMenu = new HeaderMenu(this.element);
-    this.headerGame = new HeaderGame(this.element);
   }
 
   render(): HTMLElement {
     this.rootElement.appendChild(this.element);
-    this.element.innerHTML = `
-      <div class="logo">
-        <div class="logo__top">MATCH</div>
-        <div class="logo__bottom">MATCH</div>
-      </div>
-    `;
+    this.element.appendChild(this.headerLogo.element);
     this.element.appendChild(this.headerMenu.element);
-    this.element.appendChild(this.headerGame.element);
+    new HeaderGame(this.element).render();
     return this.header;
   }
 }
