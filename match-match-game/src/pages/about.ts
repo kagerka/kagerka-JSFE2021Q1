6dfda3affd54e2204cf2d32a78e37ac0939548d9
kagerka <kagerka@gmail.com>
2021-05-28 00:38:44 +0300

    fix: change about page

--- match-match-game/src/pages/about.ts
+++ match-match-game/src/pages/about.ts
@@ -1,44 +1,31 @@
+/* eslint-disable max-len */
 import { BaseComponent } from '../components/base-components';
 import './about.scss';
+import { AboutStep } from './about_components/about_step';
 
 export class AboutPage extends BaseComponent {
   private readonly about: HTMLElement;
 
+  private readonly aboutTitle: HTMLElement;
+
+  private readonly aboutSteps: HTMLElement;
+
   constructor(private readonly rootElement: HTMLElement) {
     super('div', ['about']);
     this.about = document.createElement('div');
+    this.aboutTitle = document.createElement('h1');
+    this.aboutSteps = document.createElement('div');
+    this.aboutSteps.setAttribute('class', 'about__steps');
   }
 
   render(): HTMLElement {
     this.rootElement.innerHTML = '';
     this.rootElement.appendChild(this.element);
-    this.element.innerHTML = `
-      <h1>How to play?</h1>
-      <div class="about__steps">
-        <div class="about__step about__first-step">
-          <div class="about__text">
-            <div class="about__number">1</div>
-            <p>Register new player in game</p>
-          </div>
-          <div class="about__img"><img src="../images/about/first-step.png" alt=""></div>
-        </div>
-        <div class="about__step about__second-step">
-          <div class="about__text">
-            <div class="about__number">2</div>
-            <p>Configure your game settings</p>
-          </div>
-          <div class="about__img"><img src="../images/about/second-step.png" alt=""></div>
-        </div>
-        <div class="about__step about__third-step">
-          <div class="about__text">
-            <div class="about__number">3</div>
-            <p>Start you new game! Remember card positions and match it before times up.</p>
-          </div>
-          <div class="about__img"><img src="../images/about/third-step.png" alt=""></div>
-        </div>
-      </div>
-    `;
-
+    this.element.appendChild(this.aboutTitle).innerText = 'How to play?';
+    this.element.appendChild(this.aboutSteps);
+    new AboutStep(this.aboutSteps).render(1, 'Register new player in game', 'first-step.png');
+    new AboutStep(this.aboutSteps).render(2, 'Configure your game settings', 'second-step.png');
+    new AboutStep(this.aboutSteps).render(3, 'Start you new game! Remember card positions and match it before times up.', 'third-step.png');
     return this.about;
   }
 }
