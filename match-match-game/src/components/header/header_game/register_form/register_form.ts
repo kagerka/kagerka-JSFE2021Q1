51e734947ac869dd5a0c668bffc5e46b97de07fe
kagerka <kagerka@gmail.com>
2021-05-28 00:40:56 +0300

    feat: add validation of registered form

--- match-match-game/src/components/header/header_game/register_form/register_form.ts
+++ match-match-game/src/components/header/header_game/register_form/register_form.ts
@@ -9,20 +9,19 @@ export class RegisterForm extends BaseComponent {
 
   render(): HTMLElement {
     this.element.innerHTML = `
-
     <div class="register-form" id="register-form">
       <h1>Register new Player</h1>
-      <form action="">
+      <form id="regform">
         <div class="register-form__wrapper">
 
           <div class="register-form__items-field">
             <div class="register-form__item">
               <label for="first-name">First Name</label>
               <input type="text" placeholder="Enter your name here" name="first-name" required>
-              <div class="validate-field correct">
+              <div class="first-name validate-field correct hidden">
                 <div class="validate-field__yes">✓</div>
               </div>
-              <div class="validate-field incorrect hidden">
+              <div class="first-name validate-field incorrect">
                 <div class="validate-field__no ">!</div>
               </div>
             </div>
@@ -30,10 +29,10 @@ export class RegisterForm extends BaseComponent {
             <div class="register-form__item">
               <label for="last-name">Last Name</label>
               <input type="text" placeholder="Enter your last name here" name="last-name" required>
-              <div class="validate-field correct">
+              <div class="last-name validate-field correct hidden">
                 <div class="validate-field__yes">✓</div>
               </div>
-              <div class="validate-field incorrect hidden">
+              <div class="last-name validate-field incorrect">
                 <div class="validate-field__no ">!</div>
               </div>
             </div>
@@ -41,28 +40,34 @@ export class RegisterForm extends BaseComponent {
             <div class="register-form__item">
               <label for="email">E-mail</label>
               <input type="email" placeholder="Enter your e-mail here" name="email" required>
-              <div class="validate-field correct">
+              <div class="email validate-field correct hidden">
                 <div class="validate-field__yes">✓</div>
               </div>
-              <div class="validate-field incorrect hidden">
+              <div class="email validate-field incorrect">
                 <div class="validate-field__no ">!</div>
               </div>
             </div>
+
           </div>
           <div class="register-form__avatar">
             <div class="register-form__avatar_pic"></div>
           </div>
+
+        </div>
+        <div id="reg-form-error-msg">
+          <div id="reg-form-fname-error-msg"></div>
+          <div id="reg-form-lname-error-msg"></div>
+          <div id="reg-form-email-error-msg"></div>
         </div>
         <div class="register-form__buttons">
 
-          <button type="submit" class="btn add-user">ADD USER</button>
+          <button type="button" class="btn add-user">ADD USER</button>
           <button type="button" class="btn cancel">CANCEL</button>
 
         </div>
       </form>
     </div>
     `;
-
     return this.element;
   }
 }
