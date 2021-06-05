3cafc02267f8c186112a0446c8ca7f11fb58dec3
kagerka <kagerka@gmail.com>
2021-06-05 13:25:29 +0300

    refactor: remove unnecessary types

--- match-match-game/src/components/counter/counter.ts
+++ match-match-game/src/components/counter/counter.ts
@@ -35,8 +35,8 @@ export class Counter extends BaseComponent {
       const numOfMinutes = Math.floor(timerTime / SEC_IN_MIN);
       const numOfSeconds = timerTime % SEC_IN_MIN;
 
-      if (minutes) minutes.innerHTML = pad(numOfMinutes) as string;
-      if (seconds) seconds.innerHTML = pad(numOfSeconds) as string;
+      if (minutes) minutes.innerHTML = pad(numOfMinutes);
+      if (seconds) seconds.innerHTML = pad(numOfSeconds);
     }
 
     function startTimer(): void {
