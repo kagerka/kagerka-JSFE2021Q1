1f44dccc539bddd1c48e22d5d10b485f0b3520fe
kagerka <kagerka@gmail.com>
2021-06-07 17:49:01 +0300

    refactor: refactor validate code

--- match-match-game/src/components/header/header_game/register_form/register_lname.ts
+++ match-match-game/src/components/header/header_game/register_form/register_lname.ts
@@ -1,21 +1,76 @@
 import { BaseComponent } from '../../../base-components';
 
 export class RegisterLastName extends BaseComponent {
+  private readonly label: HTMLElement;
+
+  private readonly input: HTMLInputElement;
+
+  private readonly lnameCorrect: HTMLElement;
+
+  private readonly validateYes: HTMLElement;
+
+  private readonly lnameIncorrect: HTMLElement;
+
+  private readonly validateNo: HTMLElement;
+
   constructor(private readonly rootElement: HTMLElement) {
     super('div', ['register-form__item']);
+    this.rootElement.appendChild(this.element);
+    this.label = document.createElement('label');
+    this.label.setAttribute('for', 'last-name');
+    this.element.appendChild(this.label);
+    this.input = document.createElement('input');
+    this.input.setAttribute('type', 'text');
+    this.input.setAttribute('placeholder', 'Enter your last name here');
+    this.input.setAttribute('name', 'last-name');
+    this.input.setAttribute('required', 'required');
+    this.element.appendChild(this.input);
+    this.lnameCorrect = document.createElement('div');
+    this.lnameCorrect.setAttribute('class', 'last-name validate-field correct hidden');
+    this.element.appendChild(this.lnameCorrect);
+    this.validateYes = document.createElement('div');
+    this.validateYes.setAttribute('class', 'validate-field__yes');
+    this.lnameCorrect.appendChild(this.validateYes);
+    this.lnameIncorrect = document.createElement('div');
+    this.lnameIncorrect.setAttribute('class', 'last-name validate-field incorrect');
+    this.element.appendChild(this.lnameIncorrect);
+    this.validateNo = document.createElement('div');
+    this.validateNo.setAttribute('class', 'validate-field__no');
+    this.lnameIncorrect.appendChild(this.validateNo);
   }
 
   render(): void {
-    this.rootElement.appendChild(this.element);
-    this.element.innerHTML = `
-      <label for="last-name">Last Name</label>
-      <input type="text" placeholder="Enter your last name here" name="last-name" required>
-      <div class="last-name validate-field correct hidden">
-        <div class="validate-field__yes">✓</div>
-      </div>
-      <div class="last-name validate-field incorrect">
-        <div class="validate-field__no ">!</div>
-      </div>
-    `;
+    this.label.innerHTML = 'Last Name';
+    this.validateYes.innerHTML = '✓';
+    this.validateNo.innerHTML = '!';
+    this.lnameValidate();
+  }
+
+  lnameValidate(): void {
+    this.input.addEventListener('input', () => {
+      const lNameErrorMsg = document.getElementById('reg-form-lname-error-msg');
+      const MAX_LENGTH = 30;
+      const MIN_LENGTH = 0;
+      const lastNameValueAll = this.input.value.split('');
+      const lastNameValueIncorrect = lastNameValueAll.filter((item) => item.match('[^a-zA-Zа-яА-Я0-9 ё]'));
+      if (lastNameValueAll.length === MIN_LENGTH || lastNameValueAll.length > MAX_LENGTH
+        || lastNameValueAll.filter((item) => item.match('[0-9]')).length === lastNameValueAll.length) {
+        this.lnameCorrect.classList.add('hidden');
+        this.lnameIncorrect.classList.remove('hidden');
+        if (lNameErrorMsg) {
+          lNameErrorMsg.innerHTML = 'Your last name length should be from 1 to 30 characters and should not include only numbers.';
+        }
+      } else if (lastNameValueAll === lastNameValueIncorrect || lastNameValueIncorrect.length === MIN_LENGTH) {
+        this.lnameIncorrect.classList.add('hidden');
+        this.lnameCorrect.classList.remove('hidden');
+        if (lNameErrorMsg) { lNameErrorMsg.innerText = ''; }
+      } else {
+        this.lnameCorrect.classList.add('hidden');
+        this.lnameIncorrect.classList.remove('hidden');
+        if (lNameErrorMsg) {
+          lNameErrorMsg.innerHTML = 'Your last name should include only letters, numbers and spaces. No special characters are allowed.';
+        }
+      }
+    });
   }
 }
--- match-match-game/src/components/header/header_game/register_form/validate.ts
+++ match-match-game/src/components/header/header_game/register_form/validate.ts
@@ -2,89 +2,23 @@ import { BaseComponent } from '../../../base-components';
 
 export class ValidateForm extends BaseComponent {
   validate(): HTMLElement {
-    const fNameErrorMsg = document.getElementById('reg-form-fname-error-msg');
-    const lNameErrorMsg = document.getElementById('reg-form-lname-error-msg');
-    const emailErrorMsg = document.getElementById('reg-form-email-error-msg');
     const addUserButton = document.querySelector('.add-user');
     if (addUserButton) {
       addUserButton.setAttribute('disabled', 'disabled');
     }
     let isValid = false;
-    let isValidFirstName = false;
-    const firstName = document.querySelector('input[name="first-name"]') as HTMLInputElement;
-    const MAX_LENGTH = 30;
     const MIN_LENGTH = 0;
-    firstName?.addEventListener('input', () => {
-      const firstNameValueAll = firstName.value.split('');
-      const firstNameValueIncorrect = firstNameValueAll.filter((item) => item.match('[^a-zA-Zа-яА-Я0-9 ]'));
-      if (firstNameValueAll.length === MIN_LENGTH || firstNameValueAll.length > MAX_LENGTH
-        || firstNameValueAll.filter((item) => item.match('[0-9]')).length === firstNameValueAll.length) {
-        document.querySelector('.first-name.validate-field.correct')?.classList.add('hidden');
-        document.querySelector('.first-name.validate-field.incorrect')?.classList.remove('hidden');
-        if (fNameErrorMsg) {
-          fNameErrorMsg.innerText = 'Your first name length should be from 1 to 30 characters and should not include only numbers.';
-        }
-        isValidFirstName = false;
-      } else if (firstNameValueAll === firstNameValueIncorrect || firstNameValueIncorrect.length === MIN_LENGTH) {
-        document.querySelector('.first-name.validate-field.incorrect')?.classList.add('hidden');
-        document.querySelector('.first-name.validate-field.correct')?.classList.remove('hidden');
-        if (fNameErrorMsg) { fNameErrorMsg.innerText = ''; }
-        isValidFirstName = true;
-      } else {
-        document.querySelector('.first-name.validate-field.correct')?.classList.add('hidden');
-        document.querySelector('.first-name.validate-field.incorrect')?.classList.remove('hidden');
-        if (fNameErrorMsg) {
-          fNameErrorMsg.innerText = `
-            Your first name should include only letters, numbers and spaces. No special characters are allowed.
-          `;
-        }
-        isValidFirstName = false;
-      }
-    });
-    let isValidLastName = false;
-    const lastName = document.querySelector('input[name="last-name"]') as HTMLInputElement;
-    lastName?.addEventListener('input', () => {
-      const lastNameValueAll = lastName.value.split('');
-      const lastNameValueIncorrect = lastNameValueAll.filter((item) => item.match('[^a-zA-Zа-яА-Я0-9 ]'));
-      if (lastNameValueAll.length === MIN_LENGTH || lastNameValueAll.length > MAX_LENGTH
-        || lastNameValueAll.filter((item) => item.match('[0-9]')).length === lastNameValueAll.length) {
-        document.querySelector('.last-name.validate-field.correct')?.classList.add('hidden');
-        document.querySelector('.last-name.validate-field.incorrect')?.classList.remove('hidden');
-        if (lNameErrorMsg) { lNameErrorMsg.innerText = 'Your last name length should be from 1 to 30 characters and should not include only numbers.'; }
-        isValidLastName = false;
-      } else if (lastNameValueAll === lastNameValueIncorrect || lastNameValueIncorrect.length === MIN_LENGTH) {
-        document.querySelector('.last-name.validate-field.incorrect')?.classList.add('hidden');
-        document.querySelector('.last-name.validate-field.correct')?.classList.remove('hidden');
-        if (lNameErrorMsg) { lNameErrorMsg.innerText = ''; }
-        isValidLastName = true;
-      } else {
-        document.querySelector('.last-name.validate-field.correct')?.classList.add('hidden');
-        document.querySelector('.last-name.validate-field.incorrect')?.classList.remove('hidden');
-        if (lNameErrorMsg) { lNameErrorMsg.innerText = 'Your last name should include only letters, numbers and spaces. No special characters are allowed.'; }
-        isValidLastName = false;
-      }
-    });
-    let isValidEmail = false;
-    const email = document.querySelector('input[name="email"]') as HTMLInputElement;
-    const emailPattern = /^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,4})$/;
-    email?.addEventListener('input', () => {
-      const emailValue = email.value;
-      if (emailPattern.test(emailValue) && emailValue.length <= MAX_LENGTH) {
-        document.querySelector('.email.validate-field.incorrect')?.classList.add('hidden');
-        document.querySelector('.email.validate-field.correct')?.classList.remove('hidden');
-        if (emailErrorMsg) { emailErrorMsg.innerText = ''; }
-        isValidEmail = true;
-      } else {
-        document.querySelector('.email.validate-field.correct')?.classList.add('hidden');
-        document.querySelector('.email.validate-field.incorrect')?.classList.remove('hidden');
-        if (emailErrorMsg) { emailErrorMsg.innerText = 'Type correct e-mail address.'; }
-        isValidEmail = false;
-      }
-    });
+
     const form = document.getElementById('register-form') as HTMLInputElement;
     form?.addEventListener('input', () => {
       if (addUserButton) {
-        if (isValidFirstName && isValidLastName && isValidEmail) {
+        const fNameErrorMsg = document.getElementById('reg-form-fname-error-msg');
+        const lNameErrorMsg = document.getElementById('reg-form-lname-error-msg');
+        const emailErrorMsg = document.getElementById('reg-form-email-error-msg');
+
+        if (fNameErrorMsg?.innerText.length === MIN_LENGTH
+          && lNameErrorMsg?.innerText.length === MIN_LENGTH
+          && emailErrorMsg?.innerText.length === MIN_LENGTH) {
           isValid = true;
         }
         if (isValid) {
