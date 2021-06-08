bedabb3e95d39a967e5b277c3199083db0e61a48
kagerka <kagerka@gmail.com>
2021-06-09 01:25:04 +0300

    init: add win message component

---
+++ async-race/src/components/garage/race-field/win-msg.ts
@@ -0,0 +1,19 @@
+import { BaseComponent } from "./../../base-components";
+
+export class WinMsg extends BaseComponent {
+  private readonly winnerMessage: HTMLElement;
+
+  constructor(private readonly rootElement: HTMLElement) {
+    super('div', ['car', 'win-message']);
+    this.rootElement.appendChild(this.element);
+
+    this.winnerMessage = document.createElement('div');
+    this.winnerMessage.setAttribute('class', 'win-message_text');
+    this.element.appendChild(this.winnerMessage);
+  }
+
+  render(): HTMLElement {
+    this.winnerMessage.innerHTML = 'Lexus finished first (2.54 sec)!';
+    return this.element;
+  }
+}
