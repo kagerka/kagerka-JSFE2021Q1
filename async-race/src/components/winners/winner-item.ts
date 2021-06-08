f39f3e2aefc38a2290abddc91d85ef8aec8d6fe5
kagerka <kagerka@gmail.com>
2021-06-09 01:25:25 +0300

    init: add winner item component

---
+++ async-race/src/components/winners/winner-item.ts
@@ -0,0 +1,22 @@
+import { BaseComponent } from "./../base-components";
+
+export class WinnerItem extends BaseComponent {
+
+  constructor(private readonly rootElement: HTMLElement) {
+    super('tr', ['winners__item']);
+    this.rootElement.appendChild(this.element);
+
+  }
+
+  render(): HTMLElement {
+    this.element.innerHTML = `
+      <td>1</td>
+      <td><img src="../car.svg" style="width: 50px;" alt=""></td>
+      <td>Aston Martin 7</td>
+      <td>2</td>
+      <td>2.83</td>
+    `;
+
+    return this.element;
+  }
+}
