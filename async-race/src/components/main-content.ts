dded41c3726189da67c1c8fd4f705090e6a51151
kagerka <kagerka@gmail.com>
2021-06-09 01:21:24 +0300

    init: add main content component

---
+++ async-race/src/components/main-content.ts
@@ -0,0 +1,17 @@
+import { BaseComponent } from "./base-components";
+import { Garage } from "./garage/garage";
+import { Winners } from "./winners/winners";
+
+export class Main extends BaseComponent {
+
+  constructor(private readonly rootElement: HTMLElement) {
+    super('div', ['main']);
+    this.rootElement.appendChild(this.element);
+  }
+
+  render(): HTMLElement {
+    new Garage(this.element).render();
+    new Winners(this.element).render();
+    return this.element;
+  }
+}
