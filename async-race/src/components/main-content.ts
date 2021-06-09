2a8f36cf9dd1563918af4df77aa46015832c97e2
kagerka <kagerka@gmail.com>
2021-06-09 16:15:14 +0300

    fix: add logo to the component

--- async-race/src/components/main-content.ts
+++ async-race/src/components/main-content.ts
@@ -3,9 +3,15 @@ import { Garage } from './garage/garage';
 import { Winners } from './winners/winners';
 
 export class Main extends BaseComponent {
+  private readonly logo: HTMLElement;
+
   constructor(private readonly rootElement: HTMLElement) {
     super('div', ['main']);
     this.rootElement.appendChild(this.element);
+    this.logo = document.createElement('img');
+    this.logo.setAttribute('class', 'logo');
+    this.logo.setAttribute('src', './logo.svg');
+    this.element.appendChild(this.logo);
   }
 
   render(): HTMLElement {
