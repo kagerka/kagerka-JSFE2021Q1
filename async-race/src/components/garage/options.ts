33f0ebcc8baec56a6abdbe7cfb7bb4d49e5a1314
kagerka <kagerka@gmail.com>
2021-06-09 16:16:03 +0300

    fix: change form

--- async-race/src/components/garage/options.ts
+++ async-race/src/components/garage/options.ts
@@ -4,20 +4,27 @@ import { RaceBtnOption } from './options/race-buttons';
 import { UpdateOption } from './options/update';
 
 export class GarageOptions extends BaseComponent {
-  private readonly form: HTMLElement;
+  private readonly createForm: HTMLElement;
+
+  private readonly updateForm: HTMLElement;
 
   constructor(private readonly rootElement: HTMLElement) {
     super('div', ['garage__options']);
     this.rootElement.appendChild(this.element);
-    this.form = document.createElement('form');
-    this.form.setAttribute('id', 'options');
-    this.element.appendChild(this.form);
+
+    this.createForm = document.createElement('form');
+    this.createForm.setAttribute('id', 'create-options');
+    this.element.appendChild(this.createForm);
+
+    this.updateForm = document.createElement('form');
+    this.updateForm.setAttribute('id', 'update-options');
+    this.element.appendChild(this.updateForm);
   }
 
   render(): HTMLElement {
-    new CreateOption(this.form).render();
-    new UpdateOption(this.form).render();
-    new RaceBtnOption(this.form).render();
+    new CreateOption(this.createForm).render();
+    new UpdateOption(this.updateForm).render();
+    new RaceBtnOption(this.element).render();
     return this.element;
   }
 }
