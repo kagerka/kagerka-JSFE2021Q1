cbcb242db7e2934de0c2afff6aa82a28cbc1fbf9
kagerka <kagerka@gmail.com>
2021-06-11 00:39:28 +0300

    fix: change pagination buttons

--- async-race/src/components/pagination.ts
+++ async-race/src/components/pagination.ts
@@ -1,9 +1,13 @@
+import { getCars } from '../rest-api/garage/get-car';
 import { BaseComponent } from './base-components';
+import { FIRST_PAGE } from './constants';
+import { Race } from './garage/race-field';
 
+let pageNum = 1;
 export class Pagination extends BaseComponent {
-  private readonly prevBtn: HTMLElement;
+  public prevBtn: HTMLElement;
 
-  private readonly nextBtn: HTMLElement;
+  public nextBtn: HTMLElement;
 
   constructor(private readonly rootElement: HTMLElement) {
     super('div', ['pagination-buttons']);
@@ -14,11 +18,37 @@ export class Pagination extends BaseComponent {
     this.nextBtn = document.createElement('button');
     this.nextBtn.setAttribute('class', 'btn pagination-buttons__next');
     this.element.appendChild(this.nextBtn);
+    this.init();
   }
 
   render(): HTMLElement {
     this.prevBtn.innerText = 'Prev';
     this.nextBtn.innerText = 'Next';
     return this.element;
+  }
+
+  init(): void {
+    const listPrev = async (): Promise<void> => {
+      if (pageNum > FIRST_PAGE) {
+        pageNum--;
+      }
+      const CARS_ON_PAGE = 7;
+      const items = await getCars(pageNum, CARS_ON_PAGE);
+    };
+    const listNext = async (): Promise<void> => {
+      const CARS_ON_PAGE = 7;
+      const items = await getCars(pageNum, CARS_ON_PAGE);
+      if (pageNum < Math.floor(items.count / CARS_ON_PAGE)) {
+        pageNum++;
+      }
+    };
+    this.element.addEventListener('click', (e) => {
+      if (e.target === this.prevBtn) {
+        listPrev();
+      }
+      if (e.target === this.nextBtn) {
+        listNext();
+      }
+    });
   }
 }
