49302d670f19af8c3c58796eb0bcb97731e21669
kagerka <kagerka@gmail.com>
2021-06-11 00:40:35 +0300

    feat: add generateCarItems func

--- async-race/src/components/garage/race-field.ts
+++ async-race/src/components/garage/race-field.ts
@@ -1,37 +1,49 @@
+import { CarType } from '../../params';
+import { getCar, getCars } from '../../rest-api/garage/get-car';
 import { BaseComponent } from '../base-components';
+import { CARS_ON_PAGE, ZERO } from '../constants';
 import { RaceFieldItem } from './race-field/race-field-item';
 import { WinMsg } from './race-field/win-msg';
 
 export class Race extends BaseComponent {
-  private readonly title: HTMLElement;
+  public static title: HTMLElement;
 
-  private readonly subTitle: HTMLElement;
+  public static subTitle: HTMLElement;
 
-  private readonly raceField: HTMLElement;
+  public static raceField: HTMLElement;
 
   constructor(private readonly rootElement: HTMLElement) {
     super('div', ['garage__race']);
     this.rootElement.appendChild(this.element);
 
-    this.title = document.createElement('h2');
-    this.element.appendChild(this.title);
+    Race.title = document.createElement('h2');
+    this.element.appendChild(Race.title);
 
-    this.subTitle = document.createElement('h3');
-    this.element.appendChild(this.subTitle);
+    Race.subTitle = document.createElement('h3');
+    this.element.appendChild(Race.subTitle);
 
-    this.raceField = document.createElement('div');
-    this.raceField.setAttribute('class', 'garage__race-field');
-    this.element.appendChild(this.raceField);
+    Race.raceField = document.createElement('div');
+    Race.raceField.setAttribute('class', 'garage__race-field');
+    this.element.appendChild(Race.raceField);
   }
 
   render(): HTMLElement {
-    this.title.innerHTML = 'Garage (4)';
-    this.subTitle.innerHTML = 'Page #1';
-    new RaceFieldItem(this.raceField).render();
-    new RaceFieldItem(this.raceField).render();
-    new RaceFieldItem(this.raceField).render();
-    new RaceFieldItem(this.raceField).render();
     new WinMsg(this.element).render();
+    Race.generateCarItems();
     return this.element;
+  }
+
+  public static generateCarItems(): void {
+    let pageNum = 1;
+
+    setTimeout(async () => {
+      const items = await getCars(pageNum, CARS_ON_PAGE);
+      const itemsItems = items.items;
+      const totalCount = items.count;
+      Race.raceField.innerHTML = '';
+      itemsItems.forEach((item: CarType) => new RaceFieldItem(this.raceField).render(item.id, item.name, item.color));
+      Race.title.innerHTML = `Garage (${totalCount})`;
+      this.subTitle.innerHTML = `Page #${pageNum}`;
+    }, ZERO);
   }
 }
