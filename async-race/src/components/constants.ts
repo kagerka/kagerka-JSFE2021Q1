c04b137a0adcfb5dc1e67ba14dcb5840873d3db0
kagerka <kagerka@gmail.com>
2021-06-11 16:56:43 +0300

    fix: add new constants

--- async-race/src/components/constants.ts
+++ async-race/src/components/constants.ts
@@ -1,4 +1,8 @@
 export const CARS_ON_PAGE = 7;
 export const FIRST_PAGE = 1;
+export const ONE_HUNDRED = 100;
 export const ZERO = 0;
 export const ONE = 1;
+export const CAR_MAKE_LENGTH = 26;
+export const HEX_COLORS = 16777215;
+export const HEX = 16;
