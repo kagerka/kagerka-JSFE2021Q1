3cb8a3df58c893f01bb999ca23e332ebfc866cee
kagerka <kagerka@gmail.com>
2021-06-15 11:24:46 +0300

    fix: change winner item render

--- async-race/src/components/winners/winner-item.ts
+++ async-race/src/components/winners/winner-item.ts
@@ -1,19 +1,48 @@
 import { BaseComponent } from '../base-components';
+import { Car } from '../garage/race-field/car';
 
 export class WinnerItem extends BaseComponent {
+  private readonly rowNum: HTMLElement;
+  private readonly carView: HTMLElement;
+  private readonly winnerName: HTMLElement;
+  private readonly numOfWins: HTMLElement;
+  private readonly bestTime: HTMLElement;
+
   constructor(private readonly rootElement: HTMLElement) {
     super('tr', ['winners__item']);
     this.rootElement.appendChild(this.element);
+
+    this.rowNum = document.createElement('td');
+    this.element.appendChild(this.rowNum);
+
+    this.carView = document.createElement('td');
+    this.element.appendChild(this.carView);
+
+    this.winnerName = document.createElement('td');
+    this.element.appendChild(this.winnerName);
+
+    this.numOfWins = document.createElement('td');
+    this.element.appendChild(this.numOfWins);
+
+    this.bestTime = document.createElement('td');
+    this.element.appendChild(this.bestTime);
   }
 
-  render(): HTMLElement {
-    this.element.innerHTML = `
-      <td>1</td>
-      <td><img src="./car.svg" style="width: 50px;" alt=""></td>
-      <td>Aston Martin 7</td>
-      <td>2</td>
-      <td>2.83</td>
-    `;
+  render(color: string, id: number, rowNum: number, winnerName: string, numOfWins: number, bestTime: number): HTMLElement {
+    this.rowNum.innerHTML = `${rowNum}`;
+    this.carView.appendChild(new Car(this.carView).render(color, id));
+    this.winnerName.innerHTML = `${winnerName}`;
+    this.numOfWins.innerHTML = `${numOfWins}`;
+    this.bestTime.innerHTML = `${+(bestTime / 1000).toFixed(2)}`;
+    // this.element.innerHTML = `
+    //   <td>${rowNum}</td>
+    //   <td>
+    //     ${new Car(carView).render(id)}
+    //   </td>
+    //   <td>${winnerName}</td>
+    //   <td>${numOfWins}</td>
+    //   <td>${bestTime}</td>
+    // `;
 
     return this.element;
   }
