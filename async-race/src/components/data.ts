d833b7d0c0e2e2b51ab42198f53f54da52561472
kagerka <kagerka@gmail.com>
2021-06-16 11:45:39 +0300

    fix: add new variables

--- async-race/src/components/data.ts
+++ async-race/src/components/data.ts
@@ -3,4 +3,10 @@ export const variables = {
   animation: {},
   pageNum: 1,
   winners: [{ success: false, id: 0, time: 0 }],
+  winPageNum: 1,
+  winnersCount: '',
+  sort: 'time',
+  order: 'ASC',
+  arrowWin: '',
+  arrowTime: '',
 };
