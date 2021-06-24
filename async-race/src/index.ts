ed993fbbbd1fece96d76eb27980067a16a6d26b1
kagerka <kagerka@gmail.com>
2021-06-24 11:16:11 +0300

    refactor: refactor

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -3,8 +3,10 @@ import { Navigation } from './components/navigation';
 import './styles.scss';
 
 window.onload = (): void => {
-  const appElement = document.getElementById('app');
-  if (!appElement) throw Error('App root element not found');
+  const appElement = document.body;
+  if (!appElement) {
+    throw Error('App root element not found');
+  }
 
   new Navigation(appElement).render();
   new Main(appElement).render();
