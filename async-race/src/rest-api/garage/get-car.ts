937560a018d3ed79b8fb781bb786f8f210341225
kagerka <kagerka@gmail.com>
2021-06-24 12:15:12 +0300

    refactor: refactor functions

--- async-race/src/rest-api/garage/get-car.ts
+++ async-race/src/rest-api/garage/get-car.ts
@@ -1,22 +1,18 @@
 import {
-  CarType, GetCars, GetGarage, Params,
-} from '../../params';
+  GetCars, GetGarage, ICar,
+} from '../../models';
 import { baseUrl, path } from '../path';
 
-const generateQueryString = (queryParam: Params[]): string => (queryParam.length
-  ? `?${queryParam.map((x) => `${x.key}=${x.value}`).join('&')}`
-  : '');
-
 // получить все записи и количество машин
-export const getCarsFn = async (queryParam: Params[]): Promise<GetGarage> => {
-  const response = await fetch(`${baseUrl}${path.garage}${generateQueryString(queryParam)}`);
+export const getCarsFn = async (page: number, limit: number): Promise<GetGarage> => {
+  const response = await fetch(`${baseUrl}${path.garage}?_page=${page}&_limit=${limit}`);
   const items = await response.json();
   const carNums = Number(response.headers.get('X-Total-Count'));
   return { items, carNums };
 };
 
 // получить машинку по айди
-export const getCarFn = async (id: number): Promise<CarType> => {
+export const getCarFn = async (id: number): Promise<ICar> => {
   const response = await fetch(`${baseUrl}${path.garage}/${id}`);
   const cars = await response.json();
   return cars;
@@ -32,4 +28,4 @@ export const getCars = async (page: number, limit: number): Promise<GetCars> => 
 };
 
 // получить машину по айди
-export const getCar = async (id: number): Promise<CarType> => (await fetch(`${baseUrl}${path.garage}/${id}`)).json();
+export const getCar = async (id: number): Promise<ICar> => (await fetch(`${baseUrl}${path.garage}/${id}`)).json();
