35bdfc93d996582b6f4d0205a707c08984185cc2
kagerka <kagerka@gmail.com>
2021-06-24 20:12:20 +0300

    refactor: change update car func

--- async-race/src/rest-api/garage/update-car.ts
+++ async-race/src/rest-api/garage/update-car.ts
@@ -1,35 +1,12 @@
-import {
-  CarItem, GetGarage,
-} from '../../models';
 import { baseUrl, path } from '../path';
 
-export const updateCarFn = async (id: number, body: CarItem): Promise<GetGarage> => {
-  const response = await fetch(`${baseUrl}${path.garage}/${id}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(body),
-  });
-  const cars = await response.json();
-  return cars;
-};
-
-export const updateCarParamFn = async (id: number, body: CarItem): Promise<GetGarage> => {
+export const updateCar = async (id: number, name: string, color: string): Promise<void> => {
   const response = await fetch(`${baseUrl}${path.garage}/${id}`, {
     method: 'PATCH',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(body),
+    body: JSON.stringify({ name, color }),
   });
-  const cars = await response.json();
-  return cars;
-};
-
-export const updateCar = async (id: number, name: string, color: string): Promise<void> => {
-  await updateCarParamFn(id, {
-    name,
-    color,
-  });
+  response.json();
 };
