a6e992e0cb85086c755fbeb41add5176fff84924
kagerka <kagerka@gmail.com>
2021-06-24 20:11:55 +0300

    refactor: change get car func

--- async-race/src/rest-api/garage/get-car.ts
+++ async-race/src/rest-api/garage/get-car.ts
@@ -3,21 +3,6 @@ import {
 } from '../../models';
 import { baseUrl, path } from '../path';
 
-// получить все записи и количество машин
-export const getCarsFn = async (page: number, limit: number): Promise<GetGarage> => {
-  const response = await fetch(`${baseUrl}${path.garage}?_page=${page}&_limit=${limit}`);
-  const items = await response.json();
-  const carNums = Number(response.headers.get('X-Total-Count'));
-  return { items, carNums };
-};
-
-// получить машинку по айди
-export const getCarFn = async (id: number): Promise<ICar> => {
-  const response = await fetch(`${baseUrl}${path.garage}/${id}`);
-  const cars = await response.json();
-  return cars;
-};
-
 // получить определенное количество машин
 export const getCars = async (page: number, limit: number): Promise<GetCars> => {
   const response = await fetch(`${baseUrl}${path.garage}?_page=${page}&_limit=${limit}`);
