b15cbfea435ec57f8100bd78542a25256cec116a
kagerka <kagerka@gmail.com>
2021-07-05 22:33:14 +0300

    init: add redux

---
+++ english-for-kids/src/redux.ts
@@ -0,0 +1,127 @@
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+
+export const gameMode = createSlice({
+  name: 'gameMode',
+  initialState: {
+    value: 'train',
+  },
+  reducers: {
+    train: (state) => {
+      state.value = 'train';
+    },
+    play: (state) => {
+      state.value = 'play';
+    },
+  },
+});
+
+export const categoryName = createSlice({
+  name: 'categoryName',
+  initialState: {
+    value: 'main',
+  },
+  reducers: {
+    categoryMain: (state) => {
+      state.value = 'main';
+    },
+    categoryActionSetA: (state) => {
+      state.value = 'actionSetA';
+    },
+    categoryActionSetB: (state) => {
+      state.value = 'actionSetB';
+    },
+    categoryActionSetC: (state) => {
+      state.value = 'actionSetC';
+    },
+    categoryAdjective: (state) => {
+      state.value = 'adjective';
+    },
+    categoryAnimalSetA: (state) => {
+      state.value = 'animalSetA';
+    },
+    categoryAnimalSetB: (state) => {
+      state.value = 'animalSetB';
+    },
+    categoryFruits: (state) => {
+      state.value = 'fruits';
+    },
+    categoryVegetables: (state) => {
+      state.value = 'vegetables';
+    },
+  },
+});
+
+export const playGame = createSlice({
+  name: 'playGame',
+  initialState: {
+    value: 'correct',
+  },
+  reducers: {
+    correct: (state) => {
+      state.value = 'correct';
+    },
+    incorrect: (state) => {
+      state.value = 'incorrect';
+    },
+  },
+});
+
+export const failure = createSlice({
+  name: 'failure',
+  initialState: {
+    value: 'noError',
+  },
+  reducers: {
+    error: (state) => {
+      state.value = 'error';
+    },
+    noError: (state) => {
+      state.value = 'noError';
+    },
+  },
+});
+
+export const restoreGame = createSlice({
+  name: 'restoreGame',
+  initialState: {
+    value: 'noStart',
+  },
+  reducers: {
+    start: (state) => {
+      state.value = 'start';
+    },
+    noStart: (state) => {
+      state.value = 'noStart';
+    },
+  },
+});
+
+export const { train, play } = gameMode.actions;
+export const { correct, incorrect } = playGame.actions;
+export const { error, noError } = failure.actions;
+export const { start, noStart } = restoreGame.actions;
+export const {
+  categoryMain,
+  categoryActionSetA,
+  categoryActionSetB,
+  categoryActionSetC,
+  categoryAdjective,
+  categoryAnimalSetA,
+  categoryAnimalSetB,
+  categoryFruits,
+  categoryVegetables,
+} = categoryName.actions;
+
+export const store = configureStore({
+  reducer: {
+    gameMode: gameMode.reducer,
+    categoryName: categoryName.reducer,
+    playGame: playGame.reducer,
+    failure: failure.reducer,
+    restoreGame: restoreGame.reducer,
+  },
+});
+
+// store.subscribe(() => {
+//   console.log(store.getState());
+// });
