e02e5f70603133df62a47795ae8d0a22975ed93b
kagerka <kagerka@gmail.com>
2021-07-05 22:35:06 +0300

    fix: add gameMode state

--- english-for-kids/src/categories/category-item.ts
+++ english-for-kids/src/categories/category-item.ts
@@ -1,4 +1,5 @@
 import { BaseComponent } from '../baseComponent';
+import { store } from '../redux';
 
 export class CategoryItem extends BaseComponent {
   constructor(private readonly rootElement: HTMLElement) {
@@ -7,10 +8,17 @@ export class CategoryItem extends BaseComponent {
   }
 
   render(categoryName: string, categoryClass: string): void {
-    this.element.classList.add(categoryClass);
     this.element.innerHTML = `
       <div class="cards__card_img ${categoryClass}"></div>
-      <p>${categoryName}</p>
+      <p class="${categoryClass}">${categoryName}</p>
     `;
+    this.element.classList.add(categoryClass);
+    if (store.getState().gameMode.value === 'play') {
+      this.element.classList.add('play');
+      this.element.classList.remove('train');
+    } else {
+      this.element.classList.remove('play');
+      this.element.classList.add('train');
+    }
   }
 }
